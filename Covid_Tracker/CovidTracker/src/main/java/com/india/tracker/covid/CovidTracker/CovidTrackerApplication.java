dcf9938dba16dc30cbfc5d206c0cd37bfe7cdb27
chitranshagarwal2000 <chitranshagarwal2000@gmail.com>
2020-04-11 13:02:07 +0000

    New India Covid Tracker Added

---
+++ Covid_Tracker/CovidTracker/src/main/java/com/india/tracker/covid/CovidTracker/CovidTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.india.tracker.covid.CovidTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTrackerApplication.class, args);
+	}
+
+}
---
+++ Covid_Tracker/CovidTracker/src/test/java/com/india/tracker/covid/CovidTracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.india.tracker.covid.CovidTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
