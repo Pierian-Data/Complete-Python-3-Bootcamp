bfb144e86128f6b291a30f461ea293f7cffa098d
wutnow13 <66490530+wutnow13@users.noreply.github.com>
2020-09-17 19:16:05 -0500

    TEsting

--- 08-Milestone Project - 2/blackjack.py
+++ 08-Milestone Project - 2/blackjack.py
@@ -41,15 +41,132 @@ class Deck:
     def shuffle (self):
         random.shuffle(self.deck)
 
+class Hand:
+    def __init__(self):
+        self.deck = [] #start with an empty list
+        self.value = 0 #start with zero value
+        self.aces = 0 #add an attribute to keep track of aces
 
-    def deal (self):
+    def add_card(self,card):
         pass
-                
+
+    def adjust_for_ace(self):
+        pass
+
+
+class Chips:
+
+    def __init__(self):
+        self.total = 500
+
+        self.bet = 0
+
+    def win_bet(self):
+        pass
+
+    def lose_bet(self):
+        pass
+
+
+def take_bet():
+
+    pass #maybe use try/except. Make sure you have enough chips
+
+def hit(deck,hand):
+
+    pass #hit adds cards until the players bust
+
+def stand(deck,hand):
+
+    pass #function to stop taking cards
+
+def hit_or_stand(deck,hand):
+
+    pass #prompt the player to hit or to stand
+
+
+def show_some(player,dealer):
+
+    pass #show only some of the cards (dealer only shows 1)
+
+def show_all(player,dealer):
+
+    pass #show all of the cards in a hand
+
+def player_busts():
     
+    pass #End of Game scenario where the player busts
+
+def player_wins():
+
+    pass #End of Game scenario where the player wins
+
+def dealer_busts():
+
+    pass #End of Game scenario where the dealer busts
+
+def dealer_wins():
+
+    pass #End of Game scenario where the dealer wins
+
+def push():
+
+    pass #End of Game scenario where the dealer and player tie
+
+
+
+
 x = Deck()
 
 x.shuffle()
 
 print(x)
 
+x.deal()
 
+def play_blackjack():
+    while True:
+    # Print an opening statement
+
+    
+    # Create & shuffle the deck, deal two cards to each player
+
+    
+        
+    # Set up the Player's chips
+    
+    
+    # Prompt the Player for their bet
+
+    
+    # Show cards (but keep one dealer card hidden)
+
+        pass
+
+    #while playing:  # recall this variable from our hit_or_stand function
+        
+        # Prompt for Player to Hit or Stand
+        
+        
+        # Show cards (but keep one dealer card hidden)
+ 
+        
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        
+
+            #break
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    
+    
+        # Show all cards
+    
+        # Run different winning scenarios
+        
+    
+    # Inform Player of their chips total 
+    
+    # Ask to play again
+        
+            #pass
+        
