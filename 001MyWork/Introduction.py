cad788b0e1deba07aa07f424ae3694468e5d1140
Ankush Rathi <ankush.rathi16@gmail.com>
2021-11-15 20:36:32 +0530

    Intro

---
+++ 001MyWork/Introduction.py
@@ -0,0 +1,17 @@
+#Introduction
+print('Hi...')
+#intro end
+
+
+
+#Python 3 Basic Begin
+#1. Variable and data Structure Begin
+print('Hi')
+#1. Variable and data Structure End
+
+
+
+
+
+
+#Python 3 Basic End
