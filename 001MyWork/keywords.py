0f95c89cb3d7ba1e5fc27b66ce57dd1452c5c902
Ankush Rathi <ankush.rathi16@gmail.com>
2021-11-17 13:11:57 +0530

    keyword-basic

---
+++ 001MyWork/keywords.py
@@ -0,0 +1,20 @@
+# Print all the keyword begin
+import keyword
+print ('list of keyword :', keyword.kwlist)
+# Print all the keyword end
+
+# 1. True, False, None
+# 2. and, or, not, in, is
+# 3. for, while, break, continue
+# 4. if, else, elif
+# 5. def
+# 6. return, yield
+# 7. class
+# 8. with
+# 9. as
+# 10. pass
+# 11. lambda
+# 12. import, from
+# 13. try, except, raise, finally, assert
+# 14. del
+# 15. global, nonlocal
