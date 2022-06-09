fac6f998ed4478fde107f2b78f5c70f800a52a3f
Mattan Romano <mattaromano@gmail.com>
2022-06-08 23:45:53 -0700

    updates - 6/8

---
+++ Mattan_Workspace/error_handling.py
@@ -0,0 +1,29 @@
+'''
+
+This is a code written by Mattan on June 8, 2022. This is the multi line comment describing what's going on
+
+run this im the cmd prompt to execute review
+
+C:\Users\matta\Documents\Mattan\Visual Code Studio\Udemy\Complete-Python-3-Bootcamp\Mattan_Workspace>pylint error_handling.py -r y
+
+
+'''
+
+def my_func():
+    '''
+    
+    Describing function
+
+    '''
+
+    first = 1
+    second = 2
+
+    print(first)
+    print(second)
+
+
+if __name__ == "__main__":
+    my_func()
+else:
+    print("error")
