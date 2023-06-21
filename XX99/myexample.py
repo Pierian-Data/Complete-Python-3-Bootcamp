9e6d7577a418e3b2fd7c41cc95bf0253c1213cf7
Iskren Mitkov <iskren.mitkov@gmail.com>
2023-06-22 01:46:42 +0200

    commiting test files for string manipulation and maths testing

---
+++ XX99/myexample.py
@@ -0,0 +1,59 @@
+import datetime
+
+
+print("hello world")
+
+
+a = 2
+b = 3
+
+print(a + b)
+
+c = datetime.datetime.now()
+
+
+print(c)
+
+my_income = 100
+my_tax = 0.21
+
+my_income_tax = my_income * my_tax
+
+print(my_income_tax)
+
+my_name = 'Iskren'
+print (my_name[1:4:1])
+print (my_name[1:])
+print (my_name[:4])
+
+
+len(my_name)
+
+
+mystring = "Show me the money you bastard!"
+mystring [::-1] # reverse the string
+
+
+"Show me the money you bastard!"[2:7]
+
+
+name = 'Sam'
+last_letters = name [1:]
+newname = "P"+last_letters 
+print(newname)
+
+x = "Hello everyone!"
+print (x)
+y = x + " It is great outside!"
+print (y)
+
+letter = 'Z'
+letter * 10
+
+
+x = "Hello everyone!"
+x.upper()
+x.split()
+
+y = "Hello, this is a string and i am testing string functions!"
+y.split()
